c0e43962fce713e696fc012e585f3cd0435e86b1
NikaKlokava <veronika.kazakevich1997@gmail.com>
2023-01-20 14:38:45 +0300

    feat: add fetch request to recive weather data and save current city to localstorage

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,28 +1,156 @@
 const LOCAL_STORAGE_CITY_KEY = "current_user_city";
-const API_KEY = "";
+const API_KEY = "c1dc057239a59ed2b788017a79717d9e";
 
-function getCity() {}
+function getCity() {
+  if (localStorage.getItem(LOCAL_STORAGE_CITY_KEY)) {
+    return localStorage.getItem(LOCAL_STORAGE_CITY_KEY);
+  } else {
+    const cityMinsk = "Brest";
+    setCity(cityMinsk);
+    return cityMinsk;
+  }
+}
 
-function setCity(city) {}
+function setCity(city) {
+  localStorage.setItem(LOCAL_STORAGE_CITY_KEY, city);
+}
 
-async function loadWeather(city) {}
+async function loadWeather(city) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=en&appid=${API_KEY}&units=metric`;
+  try {
+    const promise = await fetch(url);
+    const data = await promise.json();
+    if (promise.ok) {
+      onWeatherLoadSuccess(city, data);
+    } else {
+      onWeatherLoadFailed(city);
+    }
+  } catch {
+    console.log("Error!!!");
+  }
+}
 
 function onWeatherLoadSuccess(city, data) {
-  // пока не будем реализовать
-  console.log("onWeatherLoadFailed", { data });
+  console.log("onWeatherLoadSuccess", { data });
 }
 
 function onWeatherLoadFailed(city) {
-  // пока не будем реализовать
   console.log("onWeatherLoadFailed", { city });
 }
 
 function handleCitySubmitted() {
-  // пока не будем реализовать
   console.log("handleCitySubmitted");
 }
 
 function onDOMContentLoaded() {
-  
+  setCity("");
+  const city = getCity();
+  loadWeather(city);
 }
 document.addEventListener("DOMContentLoaded", onDOMContentLoaded);
+
+
+//
+//
+//
+
+const myPromise = new Promise((resolve, reject) => {
+  try {
+    if (1 == 2) {
+      resolve("yes");
+    } else {
+      resolve("no");
+    }
+  } catch (err) {
+    reject(err);
+  }
+});
+
+async function myPromise2() {
+  try {
+    if (1 == 2) {
+      return "yes";
+    } else {
+      return "no";
+    }
+  } catch (err) {
+    throw err;
+  }
+}
+
+// async function onDOMContentLoaded() {
+//   myPromise2()
+//     .then((response) => response.json())
+//     .then((res) => {})
+//     .catch((err) => {});
+
+//   try {
+//     const res = await myPromise2();
+//   } catch (err) {}
+
+//   //   let url = `https://api.openweathermap.org/data/2.5/weather?q=Minsk&lang=en&appid=${API_KEY}&units=metric`;
+//   const response = await fetch(
+//     "https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits"
+//   );
+
+//   const res = response.json();
+//   res.then;
+
+//   //   console.log(res)
+// }
+
+// document.addEventListener("DOMContentLoaded", onDOMContentLoaded);
+
+// async function randomNum (min, max) {
+//     const random = Math.floor(Math.random() * (max - min + 1)) + min;
+//     try {
+//         if (random % 2 == 0) {
+//           return "yes";
+//         } else {
+//           return "no";
+//         }
+//       } catch (err) {
+//         throw err;
+//       }
+// }
+// const result = randomNum(3, 15);
+// const resultTotal = result.then((res) => {console.log(res)})
+// console.log(resultTotal)
+
+// const promis444 = new Promise((resolve, reject) => {
+//   const random = Math.floor(Math.random() * (max - min + 1)) + min;
+//   try {
+//     if (random % 2 == 0) {
+//       resolve("yes");
+//     } else {
+//       resolve("no");
+//     }
+//   } catch (err) {
+//     reject(err);
+//   }
+// });
+
+// promis444.then((res) => {
+//   console.log(res);
+// });
+
+// function getPromise(min, max) {
+//   return new Promise((resolve, reject) => {
+//     const random = Math.floor(Math.random() * (max - min + 1)) + min;
+//     try {
+//       if (random % 2 == 0) {
+//         resolve("yes");
+//       } else {
+//         resolve("no");
+//       }
+//     } catch (err) {
+//       reject(err);
+//     }
+//   });
+// }
+
+// async function showResult() {
+//   const result = await getPromise(1, 150);
+//   console.log(result);
+// }
+// showResult();
