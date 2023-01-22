d1eb10b12d454db497553b68b4152b7e63d9ff37
NikaKlokava <veronika.kazakevich1997@gmail.com>
2023-01-22 22:12:16 +0300

    fix: fix bugs in js code

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,6 +1,7 @@
-let audio;
+let audio; // store audio object for using it in several funcs
 let tracks = [];
-let currentIndex = 0;
+let currentIndex = 0; // used for saving current track which is loaded OR should be loaded
+let isPaused = true; // current audio playing state
 
 async function loadTrackList() {
   const url =
@@ -13,74 +14,111 @@ async function loadTrackList() {
     } else {
       onTracksLoadFailed();
     }
-  } catch {
-    console.log("Error!!!");
+  } catch (err) {
+    console.log("Error!!!", err);
   }
 }
 
 function onTracksLoadSuccess(data) {
   const trackList = document.getElementById("player-tracks");
   tracks = data;
-  for (let item of tracks) {
+
+  for (let i = 0; i < tracks.length; i++) {
     const tagP = document.createElement("p");
     tagP.className = "track-item";
-    tagP.innerHTML = item.title;
+    tagP.innerHTML = tracks[i].title;
+    tagP.onclick = () => {
+      handleTrackInPlaylistPress(i);
+    };
     trackList.append(tagP);
   }
+
   loadTrack();
 }
 
-function onTracksLoadFailed() {
-  console.error("onTracksLoadFailed");
+function onTracksLoadFailed(err) {
+  console.error("onTracksLoadFailed"); //  to do
 }
 
 function loadTrack() {
   const src = tracks[currentIndex].src;
   const url = `https://raw.githubusercontent.com/NikaKlokava/momentum-audio/main/${src}`;
+
+  // to fix ussue with dooble audio playing
+  if (!isPaused) {
+    pauseTrack();
+  }
+
   audio = new Audio(url);
 }
 
 function startTrack() {
+  audio.play();
+  isPaused = false;
+
   const currentTrackName = document.getElementById("current-track");
   const currentTitle = tracks[currentIndex].title;
   currentTrackName.innerHTML = currentTitle;
-  audio.play();
+
+  const iconPlayPause = document.getElementById("play");
+  iconPlayPause.classList.add("active");
+}
+
+function pauseTrack() {
+  audio.pause();
+  isPaused = true;
+
+  const iconPlayPause = document.getElementById("play");
+  iconPlayPause.classList.remove("active");
 }
 
 function handlePlayPausePress() {
-  const iconPlayPause = document.getElementById("play");
-  const pause = iconPlayPause.classList.toggle("active");
-  if (pause) {
-    audio.play();
+  if (isPaused) {
+    startTrack();
   } else {
-    audio.pause();
+    pauseTrack();
   }
 }
 
-function handlePlayPauseTrackPress(index) {
-  if (index == currentIndex) {
-    audio.pause();
-  } else {
+function handleTrackInPlaylistPress(index) {
+  const isCurrentTrackNotLoaded = currentIndex !== index;
+  if (isCurrentTrackNotLoaded) {
+    // if it is other track
     currentIndex = index;
     loadTrack();
     startTrack();
+  } else if (isPaused) {
+    startTrack();
+  } else {
+    pauseTrack();
   }
 }
 
 function handlePrevPress() {
-  currentIndex = currentIndex == 0 ? tracks.length - 1 : currentIndex - 1;
+  const isFirstTrackLoaded = currentIndex === 0;
+  currentIndex = isFirstTrackLoaded ? tracks.length - 1 : currentIndex - 1;
   loadTrack();
   startTrack();
 }
 
 function handleNextPress() {
-  currentIndex = currentIndex == tracks.length - 1 ? 0 : currentIndex + 1;
+  const isLastTrackLoaded = currentIndex == tracks.length - 1;
+  currentIndex = isLastTrackLoaded ? 0 : currentIndex + 1;
   loadTrack();
   startTrack();
 }
 
-async function onDOMContentLoaded() {
-  await loadTrackList();
+function onDOMContentLoaded() {
+  loadTrackList();
+
+  const iconPlayPause = document.getElementById("play");
+  iconPlayPause.onclick = handlePlayPausePress;
+
+  const prevButtonEl = document.getElementById("previous-button");
+  prevButtonEl.onclick = handlePrevPress;
+
+  const nextButtonEl = document.getElementById("next-button");
+  nextButtonEl.onclick = handleNextPress;
 }
 
 document.addEventListener("DOMContentLoaded", onDOMContentLoaded);
