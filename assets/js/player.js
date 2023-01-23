4337ed91f3d2c9451e65fd6377b256443cb1c1c4
NikaKlokava <veronika.kazakevich1997@gmail.com>
2023-01-23 16:08:29 +0300

    feat: add js func for changing volume and progress of tracks

--- assets/js/player.js
+++ assets/js/player.js
@@ -2,6 +2,7 @@ let audio; // store audio object for using it in several funcs
 let tracks = [];
 let currentIndex = 0; // used for saving current track which is loaded OR should be loaded
 let isPaused = true; // current audio playing state
+const volumeRangeInputEl = document.querySelector(".volume-progress");
 
 async function loadTrackList() {
   const url =
@@ -43,13 +44,70 @@ function onTracksLoadFailed(err) {
 function loadTrack() {
   const src = tracks[currentIndex].src;
   const url = `https://raw.githubusercontent.com/NikaKlokava/momentum-audio/main/${src}`;
-
   // to fix ussue with dooble audio playing
   if (!isPaused) {
     pauseTrack();
   }
 
   audio = new Audio(url);
+  changeVolumeLevel();
+
+  const trackTimeDuration = document.getElementById("current-time");
+  setTimeout(() => {
+    const duration = Math.round(audio.duration);
+    if (duration < 60) {
+      trackTimeDuration.innerHTML = `0:${duration}`;
+    } else {
+      trackTimeDuration.innerHTML = `1:${duration - 60}`;
+    }
+  }, 500);
+
+  const inputRangeElem = document.querySelector(".track-progress-duration");
+  audio.addEventListener("timeupdate", (event) => {
+    const duration = Math.round(audio.duration);
+    let time = Math.floor(event.timeStamp / 1000);
+    if (!isPaused) {
+      getCurrentTimeDuration(time);
+      inputRangeElem.style.width = `${(time * 100) / duration}%`;
+    } else {
+      console.log("here");
+    }
+  });
+}
+
+function changeVolumeLevel() {
+  const volumeRangeInputEl = document.querySelector(".volume-progress");
+  audio.volume = 0.5;
+  volumeRangeInputEl.value = audio.volume;
+
+  volumeRangeInputEl.addEventListener("input", () => {
+    let volumeValue = volumeRangeInputEl.value;
+    audio.volume = volumeValue;
+  });
+}
+
+function handleVolumeNoIconPress() {
+  const volumeRangeInputEl = document.querySelector(".volume-progress");
+  audio.volume = 0;
+  volumeRangeInputEl.value = audio.volume;
+}
+
+function handleVolumeYesIconPress() {
+  const volumeRangeInputEl = document.querySelector(".volume-progress");
+  audio.volume = 1;
+  volumeRangeInputEl.value = audio.volume;
+}
+
+function getCurrentTimeDuration(time) {
+  const timeOfTrackNow = document.getElementById("start-time");
+  time =
+    time < 10
+      ? (timeOfTrackNow.innerHTML = `0:0${time}`)
+      : time < 60
+      ? (timeOfTrackNow.innerHTML = `0:${time}`)
+      : time >= 60 && time < 70
+      ? (timeOfTrackNow.innerHTML = `1:0${time - 60}`)
+      : (timeOfTrackNow.innerHTML = `1:${time - 60}`);
 }
 
 function startTrack() {
@@ -119,6 +177,12 @@ function onDOMContentLoaded() {
 
   const nextButtonEl = document.getElementById("next-button");
   nextButtonEl.onclick = handleNextPress;
+
+  const volumeNoIconEl = document.querySelector(".volume-ico1");
+  volumeNoIconEl.onclick = handleVolumeNoIconPress;
+
+  const volumeYesIconEl = document.querySelector(".volume-ico2");
+  volumeYesIconEl.onclick = handleVolumeYesIconPress;
 }
 
 document.addEventListener("DOMContentLoaded", onDOMContentLoaded);
